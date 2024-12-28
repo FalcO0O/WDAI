1d462ca9c88c9924281086c7bd7cdcf168ebfcd1
FalcO0O <93099232+FalcO0O@users.noreply.github.com>
2024-12-28 02:27:09 +0100

    succesfull launch

--- lab5/my-app/src/index.tsx
+++ lab5/my-app/src/index.tsx
@@ -4,14 +4,26 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Welcome(props: { name: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | null | undefined; }) {
+    return <h1>Hello, {props.name}</h1>;
+}
+
+function Goodbye(props : {name : string | number | boolean | React.ReactElement<any, any>})
+{
+    return <h1>Goodbyes, {props.name}</h1>
+}
+
+// @ts-ignore
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const element = <Welcome name="MEEEE" />;
+
+// @ts-ignore
+const element2 = <Goodbye name="abc" />;
+
+root.render(element);
+root.render(element2);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
