5680b5d9594078065e7b4fa21779adc8c804499e
FalcO0O <93099232+FalcO0O@users.noreply.github.com>
2025-05-17 01:33:13 +0200

    cleaning

--- lab5/ex8/src/App.tsx
+++ lab5/ex8/src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import './App.css';
-import Licznik from "./components/Licznik";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Import komponentów
+import Home from './components/Home';
+import Blog from './components/Blog';
+import AddArticle from './components/AddArticle';
+import Article from './components/Article';
+import Navbar from './components/Navbar'; // opcjonalny navbar, aby nawigacja była wygodniejsza
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Licznik/>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                {/* Możesz umieścić tutaj wspólny nagłówek, np. Navbar */}
+                <Navbar />
+
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/blog" element={<Blog />} />
+                    {/* :id będzie parametrem dynamicznym dla id artykułu */}
+                    <Route path="/article/:id" element={<Article />} />
+                    <Route path="/dodaj" element={<AddArticle />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
